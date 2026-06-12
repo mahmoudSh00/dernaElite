91123165f79975f9e9de4f049219b42aedda6e5f
mahmoudSh00 <mohageer04@gmail.com>
2026-06-12 19:57:20 +0200

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
